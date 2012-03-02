1bdfe965138114c454a4e27f7901e287961f8b7f
Paul Biggar <paul.biggar@gmail.com>
2012-03-02 03:44:41 -0800

    Add a .gitignore

---
+++ project.clj
@@ -0,0 +1,19 @@
+(defproject mongofinil "0.1.0"
+  :description "A library for Mongoid-like models"
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [org.clojure/clojure-contrib "1.3-b7125d79301cbc1ce44b24c5b29e57685202041a"]
+
+                 ;; DB
+                 [congomongo "0.1.8"]
+
+                 ;; Misc
+                 [swank-clojure "1.3.4"]
+                 [clj-time "0.3.7"]
+
+                 [midje "1.3.1"]
+                 [cdt "1.2.6.1-SNAPSHOT"]]
+
+  :dev-dependencies [[lein-test-out "0.1.1"]
+                     [midje "1.3.1" :exclusions [org.clojure/clojure]]
+                     [lein-midje "1.0.7"]
+                     [clojure-source "1.2.1"]])
