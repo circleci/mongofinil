3ce32b2960832056caa423258d25f06c8edad8ee
J. David Lowe <J. David Lowe>
2013-03-11 17:53:00 -0700

    https://github.com/marick/lein-midje/issues/35 ?

--- project.clj
+++ project.clj
@@ -9,11 +9,13 @@
                  [clj-time "0.4.4"]
                  [org.clojure/tools.logging "0.2.3"]
 
+                 [bultitude "0.1.7"]
                  [lein-midje "2.0.3"]
                  [midje "1.3.1"]]
 
   :dev-dependencies [[lein-test-out "0.1.1"]
                      [midje "1.3.1" :exclusions [org.clojure/clojure]]
+                     [bultitude "0.1.7"]
                      [lein-midje "2.0.3"]
                      [bond "0.2.3" :exclusions [org.clojure/clojure]]
                      [clojure-source "1.2.1"]]
@@ -21,6 +23,7 @@
              {:dependencies
               [[lein-test-out "0.1.1"]
                [midje "1.4.0" :exclusions [org.clojure/clojure]]
+               [bultitude "0.1.7"]
                [lein-midje "2.0.3"]
                [bond "0.2.3" :exclusions [org.clojure/clojure]]]}}
 
