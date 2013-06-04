1004d15783ab41dbdd8c722457cf9d340094bcf0
Paul Biggar <paul.biggar@gmail.com>
2013-06-03 22:14:13 -0700

    Update midje

--- project.clj
+++ project.clj
@@ -10,21 +10,21 @@
                  [org.clojure/tools.logging "0.2.3"]
 
                  [bultitude "0.1.7"]
-                 [lein-midje "2.0.3"]
-                 [midje "1.3.1"]]
+                 [lein-midje "3.0.1"]
+                 [midje "1.5.1"]]
 
   :dev-dependencies [[lein-test-out "0.1.1"]
-                     [midje "1.3.1" :exclusions [org.clojure/clojure]]
+                     [midje "1.5.1" :exclusions [org.clojure/clojure]]
                      [bultitude "0.1.7"]
-                     [lein-midje "2.0.3"]
+                     [lein-midje "3.0.1"]
                      [bond "0.2.3" :exclusions [org.clojure/clojure]]
                      [clojure-source "1.2.1"]]
   :profiles {:dev
              {:dependencies
               [[lein-test-out "0.1.1"]
-               [midje "1.4.0" :exclusions [org.clojure/clojure]]
+               [midje "1.5.1" :exclusions [org.clojure/clojure]]
                [bultitude "0.1.7"]
-               [lein-midje "2.0.3"]
+               [lein-midje "3.0.1"]
                [bond "0.2.3" :exclusions [org.clojure/clojure]]]}}
 
-  :plugins [[lein-midje "2.0.3"]])
+  :plugins [[lein-midje "3.0.1"]])
