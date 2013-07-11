e6cbb1ad35d7da76a19783f52ba93707e41db075
Daniel Woelfel <dwwoelfel@gmail.com>
2013-07-10 21:08:51 -0700

    couple of tests for pre and post hooks

--- test/mongofinil/test_hooks.clj
+++ test/mongofinil/test_hooks.clj
@@ -14,21 +14,39 @@
 (defn update-hook [row]
   (update-in row [:hook-count] (fnil inc 0)))
 
+(defn pre-update-hook [query]
+  (update-in query [:pre-hooks] (fnil inc 0)))
+
 (defn load-hook [row]
   (update-in row [:loaded] (fnil inc 0)))
 
 (core/defmodel :xs
   :fields [{:name :x :findable true}]
-  :hooks {:update {:post #'update-hook}
+  :hooks {:update {:post #'update-hook
+                   :pre #'pre-update-hook}
           :load {:post #'load-hook}})
 
-(fact "hooks are triggered"
+(fact "post hooks are triggered"
   (bond/with-spy [load-hook]
     (let [orig (create! {})]
       orig => (contains {:hook-count 1})
       (-> load-hook bond/calls count) => 1)))
 
-(fact "hooks aren't called when no rows are returned"
+(fact "pre hooks are triggered"
+  (bond/with-spy [pre-update-hook]
+    (let [orig (create! {})]
+      orig => (contains {:pre-hooks 1})
+      (-> pre-update-hook bond/calls count) => 1
+      (let [update (set-fields! orig {:c 1})]
+        update => (contains {:pre-hooks 2})
+        (-> pre-update-hook bond/calls count) => 2))))
+
+(fact "post hooks aren't called when no rows are returned"
   (bond/with-spy [load-hook]
     (seq (find-one :where {:bogus 987})) => nil
     (-> load-hook bond/calls count) => 0))
+
+(fact "pre hooks aren't called when there is no query"
+  (bond/with-spy [pre-update-hook]
+    (find-one) => anything
+    (-> pre-update-hook bond/calls count) => 0))
