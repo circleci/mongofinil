635a6fbe5f103bcffe9efe5e4647d5f2e9a91b70
Conor McDermottroe <conor@circleci.com>
2019-07-17 15:08:11 +0100

    Test with many Clojure and MongoDB versions
    Test with every permutation of Clojure and MongoDB we expect the library
    to work with.

--- project.clj
+++ project.clj
@@ -2,7 +2,8 @@
   :description "A library for Mongoid-like models"
   :dependencies [[congomongo "1.1.0"]
                  [clj-time "0.14.0"]]
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.8.0"]
+  :profiles {:dev {:dependencies [[org.clojure/clojure ~(or (System/getenv "CLOJURE_VERSION")
+                                                            "1.8.0")]
                                   [circleci/bond "0.3.1"]
                                   [midje "1.9.9" :exclusions [clj-time
                                                               org.clojure/clojure]]]}}
